bd4ec25a18fe8ff14357360ddc53bf29fdfc423c
shweta <shwetabirajdar5872gmail.com>
2022-09-10 10:46:53 +0530

    Added switch case

--- EmpWage.java
+++ EmpWage.java
@@ -6,25 +6,35 @@ public class EmpWage {
 		int fullTime = 1;
 		int perHrsWage = 20;
 		int partTime = 2;
+		int monthSal = 0;
+		int salary;
+		for (int i = 1; i <= 20; i++) {
 
-		double check = Math.floor(Math.random() * 10) % 3;
-		if (check == fullTime) {
-			System.out.println("Employee is present");
-			int Hrs = 8;
-			int salary = perHrsWage * Hrs;
-			System.out.println("Salary : " + salary);
+			int check = (int) Math.floor(Math.random() * 10) % 3;
+			switch (check) {
+			
+			case 1:
+				System.out.println("Employee is present in part Time");
+				int Hrs = 8;
+				salary = perHrsWage * Hrs;
+				System.out.println("salary : " + salary);
+				break;
+			case 2:
+				System.out.println("Employee is present in part Time");
+				Hrs = 4;
+				salary = perHrsWage * Hrs;
+				System.out.println("salary : " + salary);
+				break;
+			default:
+				System.out.println("Employee is present in part Time");
+				Hrs = 0;
+				salary = perHrsWage * Hrs;
+				System.out.println("salary : " + salary);
+				break;
+			}
+			monthSal = salary + monthSal;
+			
 		}
-		if(check == partTime) {
-			System.out.println("Employee is present in part Time");
-			int Hrs = 8;
-			int salary = partTime * Hrs;
-			System.out.println("salary : " + salary);
-		}
-		else {
-			System.out.println("Employee is absent");
-			int Hrs = 0;
-			int salary = perHrsWage * Hrs;
-			System.out.println("Salary : " + salary);
-		}
+		System.out.println(monthSal);
 	}
 }

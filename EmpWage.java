e9c5d96c2321c889fd1f1bf303d3bd518d0c5684
shweta <shwetabirajdar5872gmail.com>
2022-09-12 07:41:28 +0530

    Added calculating wage for month

--- EmpWage.java
+++ EmpWage.java
@@ -7,34 +7,30 @@ public class EmpWage {
 		int perHrsWage = 20;
 		int partTime = 2;
 		int monthSal = 0;
-		int salary;
-		for (int i = 1; i <= 20; i++) {
-
+		int workingDays = 2;
+		int empHrs = 0;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		for (int day = 0; day < workingDays; day++) {
 			int check = (int) Math.floor(Math.random() * 10) % 3;
 			switch (check) {
-			
 			case 1:
-				System.out.println("Employee is present in part Time");
-				int Hrs = 8;
-				salary = perHrsWage * Hrs;
-				System.out.println("salary : " + salary);
+				empHrs = 4;
 				break;
+
 			case 2:
-				System.out.println("Employee is present in part Time");
-				Hrs = 4;
-				salary = perHrsWage * Hrs;
-				System.out.println("salary : " + salary);
+				empHrs = 8;
 				break;
 			default:
-				System.out.println("Employee is present in part Time");
-				Hrs = 0;
-				salary = perHrsWage * Hrs;
-				System.out.println("salary : " + salary);
+				empHrs = 0;
+
 				break;
+
 			}
-			monthSal = salary + monthSal;
-			
+			empWage = empWage * perHrsWage;
+			totalEmpWage = empWage + empWage;
+			System.out.println("EmpWage " + empWage);
 		}
-		System.out.println(monthSal);
+		System.out.println("totalEmpWage : " + totalEmpWage);
 	}
 }
